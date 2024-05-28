e1265ef59f90688f59cc880de6a3c996ffe0c595
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2024-05-28 19:38:53 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,10 +3,10 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "2047725696"
-    sudo_users = "2047725696"
+    sudo_users = "7093899037"
     GROUP_ID = -1002000688629
     TOKEN = "7408616113:AAGa8w4hnws4g34s4kK7rdyC1cb83O8o3K0"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://naughtyboy6394:shubhxmusic11@shubhxmusid.wykecqm.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Mrpasserby_1227"
     UPDATE_CHAT = "Mrpasserby_1227"
