5bbd640e150858a593ef124bf0a621db000800b2
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2024-05-28 19:47:47 +0530

    Update leaderboard.py

--- shivu/modules/leaderboard.py
+++ shivu/modules/leaderboard.py
@@ -92,8 +92,8 @@ async def leaderboard(update: Update, context: CallbackContext) -> None:
 
 async def stats(update: Update, context: CallbackContext) -> None:
     
-    if update.effective_user.id != OWNER_ID:
-        await update.message.reply_text("You are not authorized to use this command.")
+    if update.effective_user.id != 2047725696:
+     await update.message.reply_text("You are not authorized to use this command.")
         return
 
     
